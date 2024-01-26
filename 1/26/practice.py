458374fd6696e83f3b556dc612b85e664ead020b
Sisi Small <smallsianah@gmail.com>
2024-01-26 13:37:23 -0500

    added tasks and student_scores.npy

---
+++ 1/26/practice.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+student_scores = np.load("student_scores.npy")
+#functions: np.sum, np.sort, np.flip, np.mean, np.min, np.max,
+
+print(student_scores)
+
+#Task 1: Compute every student's mean assignment score
+#Task 2: Compute the mean score for each assignmnet(across all students)
+#Task 3: Compute the global mean score
+#Task 4: Drop the lowest assignment grade for each studnet, then compute their mean assignment score
+#Task 5: Report the average scores of the top-3 assignments in which the students performed best on average
