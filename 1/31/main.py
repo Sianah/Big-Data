9d7e115cc9c6532f21bc828bedcf889d2eaddaea
Sisi Small <smallsianah@gmail.com>
2024-01-31 14:15:37 -0500

    added excel sheet to read

--- 1/31/main.py
+++ 1/31/main.py
@@ -14,10 +14,16 @@ df = pd.read_csv(filepath_or_buffer="hd2022.csv", encoding='latin1')
 # print(df['INSTNM'])
 # print(df[df['INSTNM']=='Stetson University']) #will not display which rows that don't correlate to what you're looking for
 
-stetson = df[df['INSTNM']=='Stetson University'].iloc[0]
+stetson = df[df['INSTNM']=='Stetson University']#.iloc[0]
 # rich.print(stetson.to_dict()) #shows all relevant information pertaining to the school
 
 df_finaid = pd.read_csv(filepath_or_buffer="sfa2122.csv",encoding='latin1')
+df_finaid_dictionary = pd.read_excel("sfa2122.xlsx",sheet_name="varlist")
 print(df_finaid)
-stetson_finaid = df_finaid[stetson['UNITID'].iloc[0] == df_finaid['UNITID']].iloc[0] #iloc = index location
-print(stetson_finaid.to_dict())
+print(df_finaid_dictionary)
+stetson_finaid = df_finaid[stetson['UNITID'] == df_finaid['UNITID']]#.iloc[0] #iloc = index location
+# print(stetson_finaid.to_dict())
+stetson_finaid_newkeys = {}
+for key in stetson_finaid:
+    new_key = df_finaid_dictionary[df_finaid_dictionary['varname'] == key]['varTitle']#.iloc[0]
+    stetson_finaid_newkeys[new_key] = stetson_finaid[key]
