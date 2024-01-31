4944abf3bd56280f8dd3998f4ca03033b189ffba
Sisi Small <smallsianah@gmail.com>
2024-01-31 14:07:01 -0500

    using another dataset with iloc

--- 1/31/main.py
+++ 1/31/main.py
@@ -5,14 +5,19 @@ import pandas as pd
 import rich
 
 df = pd.read_csv(filepath_or_buffer="hd2022.csv", encoding='latin1')
-print(df)
-print(df.columns)
-print(df.shape)
-# print(df['UNITID'])
-# print(type(df['UNITID']))
-# print(len(df['UNITID']))
-print(df['INSTNM'])
-print(df[df['INSTNM']=='Stetson University']) #will not display which rows that don't correlate to what you're looking for
+# print(df)
+# print(df.columns)
+# print(df.shape)
+# # print(df['UNITID'])
+# # print(type(df['UNITID']))
+# # print(len(df['UNITID']))
+# print(df['INSTNM'])
+# print(df[df['INSTNM']=='Stetson University']) #will not display which rows that don't correlate to what you're looking for
 
-stetson = df[df['INSTNM']=='Stetson University']
-rich.print(stetson.to_dict()) #shows all relevant information pertaining to the school
+stetson = df[df['INSTNM']=='Stetson University'].iloc[0]
+# rich.print(stetson.to_dict()) #shows all relevant information pertaining to the school
+
+df_finaid = pd.read_csv(filepath_or_buffer="sfa2122.csv",encoding='latin1')
+print(df_finaid)
+stetson_finaid = df_finaid[stetson['UNITID'].iloc[0] == df_finaid['UNITID']].iloc[0] #iloc = index location
+print(stetson_finaid.to_dict())
