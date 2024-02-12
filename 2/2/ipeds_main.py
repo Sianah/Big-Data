1014639f13920efe802f62045cc7b61ebf944faf
Sisi Small <smallsianah@gmail.com>
2024-02-12 14:20:05 -0500

    took notes on different types of graphing methods

--- 2/2/ipeds_main.py
+++ 2/2/ipeds_main.py
@@ -36,14 +36,15 @@ rich.print(fl_univ.loc[651])  # pulls out data from that row
 # fl_univ.index = fl_univ['UNITID'] #unitID is now the row name
 fl_univ.index = fl_univ['INSTNM']
 rich.print(fl_univ)
-rich.print(fl_univ.loc['Stetson University'])
+rich.print(fl_univ.loc['Stetson University'])  # pulls out data from that row
 # remember, iloc always needs a number. Loc does not. iloc takes row integer, loc takes name
 # rich.print(fl_univ ['Stetson University']) #Would look for a column named stetson university
 # will gather street address of stetson. At will isolate row column
+# at is like loc, but faster
 rich.print(fl_univ.at['Stetson University', 'ADDR'])
 
 rich.print(fl_univ.describe())  # takes every column, counts records
-rich.print(fl_univ[['ADDR', 'CITY']].count())
+rich.print(fl_univ[['ADDR', 'CITY']].count())  # counts records for each column
 
 
 # df_finaid = pd.read_csv("sfa2122.csv", encoding='latin1')
