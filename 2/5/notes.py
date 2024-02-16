165dc5c84cf0cd1f8c2d1d6ff4cab8061d1983ff
Sisi Small <smallsianah@gmail.com>
2024-02-16 14:20:22 -0500

    included nyctaxi file

--- 2/5/notes.py
+++ 2/5/notes.py
@@ -1,8 +1,8 @@
-#notes for cinf401
-#cd cinf401
-#ls
-#python -m virtualenv venv #creates a virtual environment
-#source venv/bin/activate #activates the virtual environment #what you do everytime you start a new terminal/comes back to server
-#pip install pandas #installs pandas
-#vim nyctaxi.py #opens a file
-#python nyctaxi.py #runs the file
+# notes for cinf401
+# cd cinf401
+# ls
+# python -m virtualenv venv #creates a virtual environment
+# source venv/bin/activate #activates the virtual environment #what you do everytime you start a new terminal/comes back to server
+# pip install pandas #installs pandas
+# vim nyctaxi.py #opens a file
+# python nyctaxi.py #runs the file
